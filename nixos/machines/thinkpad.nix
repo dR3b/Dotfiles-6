b0b26497dfa524ce43ce335dee19d99e69584e08
Marek Fajkus <marek.faj@gmail.com>
2019-05-08 10:15:23 +0200

    remove commented stuff

--- nixos/machines/thinkpad.nix
+++ nixos/machines/thinkpad.nix
@@ -89,11 +89,6 @@
     #configFile = pkgs.writeText "default.pa" ''
       #load-module module-bluetooth-policy
       #load-module module-bluetooth-discover
-      ### module fails to load with
-      ###   module-bluez5-device.c: Failed to get device path from module arguments
-      ###   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
-      ## load-module module-bluez5-device
-      ## load-module module-bluez5-discover
     #'';
   };
 
